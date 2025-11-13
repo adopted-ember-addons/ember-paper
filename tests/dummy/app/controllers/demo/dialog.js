5257a1cbfb39d0516ecea4b0023341de930048ae
Chris Manson <chris@manson.ie>
2025-11-13 17:04:52 +0000

    fix dialog demo

--- tests/dummy/app/controllers/demo/dialog.js
+++ tests/dummy/app/controllers/demo/dialog.js
@@ -1,58 +1,60 @@
-/* eslint-disable ember/no-actions-hash, prettier/prettier */
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  actions: {
+export default class extends Controller {
+  @tracked dialogOrigin;
+  @tracked showDialogWithParent;
+  @tracked result;
+  @tracked showDialog;
+  @tracked showPromptDialog;
+  @tracked dogName = '';
+  @tracked showAnimatedDialog;
 
-    /* Dialog with parent */
-    openDialogWithParent(event) {
-      this.set('dialogOrigin', event.currentTarget);
-      this.set('showDialogWithParent', true);
-    },
+  /* Dialog with parent */
+  openDialogWithParent = (event) => {
+    this.dialogOrigin = event.currentTarget;
+    this.showDialogWithParent = true;
+  };
 
-    closeDialogWithParent(result) {
-      this.set('result', result);
-      this.set('showDialogWithParent', false);
-    },
+  closeDialogWithParent = (result) => {
+    this.result = result;
+    this.showDialogWithParent = false;
+  };
 
-    /* Dialog */
-    openDialog(event) {
-      this.set('dialogOrigin', event.currentTarget);
-      this.set('showDialog', true);
-    },
+  /* Dialog */
+  openDialog = (event) => {
+    this.dialogOrigin = event.currentTarget;
+    this.showDialog = true;
+  };
 
-    closeDialog(result) {
-      this.set('result', result);
-      this.set('showDialog', false);
-    },
+  closeDialog = (result) => {
+    this.result = result;
+    this.showDialog = false;
+  };
 
-    /* Prompt dialog */
-    dogName: '',
-    openPromptDialog(/* param, event */) {
-      this.set('dialogOrigin', null);
-      this.set('showPromptDialog', true);
-    },
+  openPromptDialog = (/* param, event */) => {
+    this.dialogOrigin = null;
+    this.showPromptDialog = true;
+  };
 
-    closePromptDialog(result, dogName) {
-      if (result === 'ok') {
-        result = `${result} and dog named ${dogName}`;
-      }
-      this.set('result', result);
-      this.set('showPromptDialog', false);
-    },
-
-    toggleSourceCode() {
-      this.toggleProperty('showSourceCode');
-    },
-
-    /* Animated dialog */
-    openAnimatedDialog() {
-      this.set('showAnimatedDialog', true);
-    },
-
-    closeAnimatedDialog() {
-      this.set('showAnimatedDialog', false);
+  closePromptDialog = (result, dogName) => {
+    if (result === 'ok') {
+      result = `${result} and dog named ${dogName}`;
     }
+    this.result = result;
+    this.showPromptDialog = false;
+  };
 
-  }
-});
+  toggleSourceCode = () => {
+    this.showSourceCode = !this.showSourceCode;
+  };
+
+  /* Animated dialog */
+  openAnimatedDialog = () => {
+    this.showAnimatedDialog = true;
+  };
+
+  closeAnimatedDialog = () => {
+    this.showAnimatedDialog = false;
+  };
+}
