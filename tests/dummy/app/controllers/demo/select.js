0c3782b0fcc651dcb1a02f79494084bf1b0c0a1c
Chris Manson <chris@manson.ie>
2025-11-13 10:38:25 +0000

    fix the select demo

--- tests/dummy/app/controllers/demo/select.js
+++ tests/dummy/app/controllers/demo/select.js
@@ -1,40 +1,38 @@
-/* eslint-disable ember/require-computed-property-dependencies, prettier/prettier */
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
 import RSVP from 'rsvp';
 import { A } from '@ember/array';
 import { later } from '@ember/runloop';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
+export default class extends Controller {
+  states =
+    'AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY'
+      .split(' ')
+      .map((state) => ({ abbrev: state }));
 
-  states: computed(function() {
-    return A('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY'
-      .split(' ').map((state) => ({ abbrev: state })));
-  }),
+  @tracked
+  userState = this.states[4];
 
-  userState: computed(function() {
-    return this.states.objectAt(4);
-  }),
-
-  sizes: A([
+  sizes = [
     'small (12-inch)',
     'medium (14-inch)',
     'large (16-inch)',
-    'insane (42-inch)'
-  ]),
+    'insane (42-inch)',
+  ];
 
-  vegetables: A([
+  vegetables = [
     { name: 'Corn', checked: false },
     { name: 'Onions', checked: false },
     { name: 'Kale', checked: false },
     { name: 'Arugula', checked: false },
     { name: 'Peas', checked: false },
-    { name: 'Zucchini', checked: false }
-  ]),
+    { name: 'Zucchini', checked: false },
+  ];
+
   /*
    * Fake promise to fetch data, here you would use ember-data, jQuery.ajax or whatever you want.
    */
-  users: computed(function() {
+  get users() {
     return new RSVP.Promise((resolve) => {
       // Just wait for 800ms to 2 seconds for a fake progress, so it feels like a query.
       let waitMS = Math.floor(Math.random() * 2000) + 800;
@@ -44,7 +42,7 @@ export default Controller.extend({
         { id: 2, name: 'Shaggy Rodgers' },
         { id: 3, name: 'Fred Jones' },
         { id: 4, name: 'Daphne Blake' },
-        { id: 5, name: 'Velma Dinkley' }
+        { id: 5, name: 'Velma Dinkley' },
       ]);
 
       later(() => {
@@ -55,11 +53,17 @@ export default Controller.extend({
          */
         resolve(dataFromServer);
       }, waitMS);
-
     });
-  }),
-  groupedToppings: Object.freeze([
-    { groupName: 'Meats', options: ['Pepperoni', 'Sausage', 'Ground Beef', 'Bacon'] },
-    { groupName: 'Veg', options: ['Mushrooms', 'Onion', 'Green Pepper', 'Green Olives'] }
-  ])
-});
+  }
+
+  groupedToppings = [
+    {
+      groupName: 'Meats',
+      options: ['Pepperoni', 'Sausage', 'Ground Beef', 'Bacon'],
+    },
+    {
+      groupName: 'Veg',
+      options: ['Mushrooms', 'Onion', 'Green Pepper', 'Green Olives'],
+    },
+  ];
+}
