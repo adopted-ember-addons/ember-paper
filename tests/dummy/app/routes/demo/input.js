fe5cb6a9b199a59c4cb11091b721ed95bedb8287
Chris Manson <chris@manson.ie>
2025-11-13 16:56:29 +0000

    fix input demo

--- tests/dummy/app/routes/demo/input.js
+++ tests/dummy/app/routes/demo/input.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
+import { tracked } from 'tracked-built-ins';
 
 export default class extends Route {
   model() {
-    return {};
+    return tracked({});
   }
 }
