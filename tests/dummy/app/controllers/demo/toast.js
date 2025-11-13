9f6ac99ba30eb6e390bf0588b15f2d3d8b9bddde
Chris Manson <chris@manson.ie>
2025-11-13 15:54:47 +0000

    fix toast demo

--- tests/dummy/app/controllers/demo/toast.js
+++ tests/dummy/app/controllers/demo/toast.js
@@ -1,60 +1,61 @@
-/* eslint-disable ember/no-actions-hash, prettier/prettier */
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  duration: 3000,
-  positionX: 'left',
-  positionY: 'bottom',
-  toastText: 'Hello world',
-  toastClass: '',
+export default class extends Controller {
+  @tracked duration = 3000;
+  @tracked positionX = 'left';
+  @tracked positionY = 'bottom';
+  @tracked toastText = 'Hello world';
+  @tracked toastClass = '';
+  @tracked showToast = false;
+  @tracked showToastWithout = false;
+  @tracked showSourceCode = false;
 
-  paperToaster: service(),
+  @service paperToaster;
 
-  actions: {
-    /* Toast */
-    openToast() {
-      this.set('showToast', true);
-    },
-    openToastWithout() {
-      this.set('showToastWithout', true);
-    },
-    // BEGIN-SNIPPET toaster
-    openServiceToast() {
-      this.paperToaster.show(this.toastText, {
-        duration: 4000,
-        toastClass: this.toastClass
-      });
-    },
+  /* Toast */
+  openToast = () => {
+    this.showToast = true;
+  };
+  openToastWithout = () => {
+    this.showToastWithout = true;
+  };
+  // BEGIN-SNIPPET toaster
+  openServiceToast = () => {
+    this.paperToaster.show(this.toastText, {
+      duration: 4000,
+      toastClass: this.toastClass,
+    });
+  };
 
-    openServiceActionToast() {
-      this.paperToaster.show(this.toastText, {
-        duration: 4000,
-        toastClass: this.toastClass,
-        action: {
-          label: 'Undo',
-          accent: true,
-          onClick() {
-            alert('toast action pressed');
-          }
-        }
-      });
-    },
-    // END-SNIPPET
-    cancelToast(toast) {
-      this.paperToaster.cancelToast(toast);
-    },
-    closeToast() {
-      this.set('showToast', false);
-    },
-    closeToastWithout() {
-      this.set('showToastWithout', false);
-    },
-    toggleSourceCode() {
-      this.toggleProperty('showSourceCode');
-    },
-    buttonAction() {
-      alert('You have pressed the button!');
-    }
+  openServiceActionToast = () => {
+    this.paperToaster.show(this.toastText, {
+      duration: 4000,
+      toastClass: this.toastClass,
+      action: {
+        label: 'Undo',
+        accent: true,
+        onClick() {
+          alert('toast action pressed');
+        },
+      },
+    });
+  };
+  // END-SNIPPET
+  cancelToast(toast) {
+    this.paperToaster.cancelToast(toast);
   }
-});
+  closeToast = () => {
+    this.showToast = false;
+  };
+  closeToastWithout = () => {
+    this.showToastWithout = false;
+  };
+  toggleSourceCode = () => {
+    this.showSourceCode = !this.showSourceCode;
+  };
+  buttonAction() {
+    alert('You have pressed the button!');
+  }
+}
