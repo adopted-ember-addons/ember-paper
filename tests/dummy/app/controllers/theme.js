63ed6d4620efa282e9464d95299d932cf5329807
Chris Manson <chris@manson.ie>
2025-11-13 13:53:21 +0000

    fix theme route

--- tests/dummy/app/controllers/theme.js
+++ tests/dummy/app/controllers/theme.js
@@ -1,52 +1,63 @@
-/* eslint-disable ember/no-actions-hash, prettier/prettier */
+/* eslint-disable prettier/prettier */
 import Controller from '@ember/controller';
-import { inject } from '@ember/service';
-import { and, not } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 import PALETTES from 'ember-paper/utils/palettes';
-import { computed } from '@ember/object';
+import { action } from '@ember/object';
 
-export default Controller.extend({
-  paperTheme: inject(),
-
-  palettes: computed(function() {
-    return Object.keys(PALETTES).map((key) => {
-      let palette = PALETTES[key];
-      return {
-        name: key,
-        palette
-      };
-    });
-  }),
-
-  isValid: and('primary', 'accent', 'warn'),
-  isInvalid: not('isValid'),
-
-  primary: computed('palettes.@each.name', function() {
-    return this.palettes.find((p) => p.name === 'pink');
-  }),
-
-  accent: computed('palettes.@each.name', function() {
-    return this.palettes.find((p) => p.name === 'green');
-  }),
-
-  warn: computed('palettes.@each.name', function() {
-    return this.palettes.find((p) => p.name === 'red');
-  }),
-
-  actions: {
-    // BEGIN-SNIPPET theme.service
-    installTheme() {
-      this.paperTheme.installTheme('main', {
-        primary: this.primary.palette,
-        accent: this.accent.palette,
-        warn: this.warn.palette
-      });
-    },
-
-    uninstallTheme() {
-      this.paperTheme.uninstallTheme('main');
-    }
-    // END-SNIPPET
-  }
+const palletObjects = Object.keys(PALETTES).map((key) => {
+  let palette = PALETTES[key];
+  return {
+    name: key,
+    palette
+  };
 });
+
+export default class extends Controller {
+  @service paperTheme;
+
+  @tracked primary = palletObjects.find((p) => p.name === 'pink');
+  @tracked accent = palletObjects.find((p) => p.name === 'green');
+  @tracked warn = palletObjects.find((p) => p.name === 'red');
+
+  constructor() {
+    super(...arguments);
+  }
+
+  get palettes() {
+    return palletObjects;
+  }
+
+  get isValid() {
+    return this.primary && this.accent && this.warn
+  }
+
+  get isInvalid() {
+    return !this.isValid
+  }
+
+
+
+  // BEGIN-SNIPPET theme.service
+  @action
+  installTheme() {
+    this.paperTheme.installTheme('main', {
+      primary: this.primary.palette,
+      accent: this.accent.palette,
+      warn: this.warn.palette
+    });
+  }
+
+  @action
+  uninstallTheme() {
+    this.paperTheme.uninstallTheme('main');
+  }
+  // END-SNIPPET
+
+
+  @action
+  setTheme(type, value) {
+    this[type] = value;
+  }
+}
