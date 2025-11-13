008c91da6dd63339313b3ba8933065d810037cc2
Chris Manson <chris@manson.ie>
2025-11-13 16:42:37 +0000

    fix progress-circular demo

--- tests/dummy/app/controllers/demo/progress-circular.js
+++ tests/dummy/app/controllers/demo/progress-circular.js
@@ -1,51 +1,47 @@
-/* eslint-disable ember/no-actions-hash, ember/no-runloop, prettier/prettier */
 import Controller from '@ember/controller';
-import { later, cancel } from '@ember/runloop';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  mode: 'query',
-  determinateValue: 0,
-  timer: null,
+export default class extends Controller {
+  @tracked mode = 'query';
+  @tracked determinateValue = 0;
+  @tracked timer = null;
 
   start() {
-    this.set('determinateValue', 30);
+    this.determinateValue = 30;
     this.setupTimer();
-  },
-
-  setupTimer() {
-    this.set('timer', later(this, function() {
-      let value = this.incrementProperty('determinateValue', 1);
-      if (value > 100) {
-        this.set('determinateValue', 30);
-      }
-      this.setupTimer();
-    }, 100));
-  },
-
-  stop() {
-    cancel(this.timer);
-  },
-
-  sliderDiameter: 100,
-  sliderValue: null,
-  isIndeterminate: true,
-  strokeRatio: 0.1,
-
-  actions: {
-    setValue(v) {
-      this.set('isIndeterminate', false);
-      this.set('sliderValue', v);
-    },
-
-    setIndeterminate(value) {
-      if (value) {
-        this.set('isIndeterminate', true);
-        this.set('sliderValue', null);
-      } else {
-        this.set('isIndeterminate', false);
-        this.set('sliderValue', 50);
-      }
-    }
   }
 
-});
+  setupTimer() {
+    this.timer = setTimeout(() => {
+      this.determinateValue++;
+      if (this.determinateValue > 100) {
+        this.determinateValue = 30;
+      }
+      this.setupTimer();
+    }, 100);
+  }
+
+  stop() {
+    clearTimeout(this.timer);
+  }
+
+  @tracked sliderDiameter = 100;
+  @tracked sliderValue = null;
+  @tracked isIndeterminate = true;
+  @tracked strokeRatio = 0.1;
+
+  setValue = (v) => {
+    this.isIndeterminate = false;
+    this.sliderValue = v;
+  };
+
+  setIndeterminate = (value) => {
+    if (value) {
+      this.isIndeterminate = true;
+      this.sliderValue = null;
+    } else {
+      this.isIndeterminate = false;
+      this.sliderValue = 50;
+    }
+  };
+}
